06fa6592a4fe457e51eae9928563f21e75e36e2a
b3rnix <bpanarello@gmail.com>
2017-08-10 14:45:22 -0300

    Slurm script

--- py/dbsession.py
+++ py/dbsession.py
@@ -1,8 +1,37 @@
 from neo4j.v1 import GraphDatabase, basic_auth
+import re
+
 
 driver = GraphDatabase.driver("bolt://localhost:7687", auth=basic_auth("neo4j", "otwauk"))
 
 sess = driver.session()
+
+slurmuriregex  = re.compile('slurm:\/\/c:([a-zA-Z0-9]+)\/p:([a-zA-Z0-9]+)\/a:([a-zA-Z0-9]+)')
+def get_slurm_data_from_uri(uri):
+    m = slurmuriregex.match(uri)
+    if m == None:
+        raise Exception("Invalid SLURM URI!!")
+
+    return {
+        "cluster": m.groups()[0],
+        "partition": m.groups()[1],
+        "account": m.groups()[2]
+    }
+
+def get_contract(contract_id):
+    sess = driver.session()
+    result = sess.run("MATCH (c:CONTRACT) WHERE c.id = {id} "
+                           "RETURN c",
+                           {"id": contract_id})
+
+    data = result.data()
+    sess.close()
+    if (len(data) == 0):
+        raise Exception("Unregistered contract")
+
+    return data[0].values()[0]
+
+
 
 def create_hpc_node(cluster, node_id, host_name, capacity):
     sess = driver.session()
@@ -86,7 +115,7 @@ def calculatePathWeight(path):
     factor = reduce(lambda fac,share: fac * share, shares,1.0)
     return factor
 
-def get_available_capacity_for_contract(contract_id=None,uri=None):
+def get_available_capacity_for_contract(contract_id=None):
     # Each contract contains one or more paths to the resources connected directly or indirectly to it.(i.e. a path connect a single
     # contract to a single resource)
     # Each path has a weight calculated as the product of the attribute "share" corresponding to
@@ -96,16 +125,13 @@ def get_available_capacity_for_contract(contract_id=None,uri=None):
     # as the node's capacity times the path's capacity (which is always a number between 0 and 1).
 
 
-    if (uri == None and contract_id == None):
-        raise Exception("Must specify at least one contract identification (id or uri)")
-
     #MATCH (c:CONTRACT{id:"hpc_bancarizada"}) MATCH (n:HPCNODE)  RETURN (c)-[*..1]->(n);
     sess = driver.session()
     result = sess.run(
-        "MATCH (c:CONTRACT{id:{pcontract_id},uri:{puri}}) MATCH (n:HPCNODE)  RETURN (c)-[*..6]->(n)",
+        "MATCH (c:CONTRACT{id:{pcontract_id}}) MATCH (n:HPCNODE)  RETURN (c)-[*..6]->(n)",
         {
-            "pcontract_id": contract_id,
-            "uri": uri
+            "pcontract_id": contract_id
+
         }
     )
     data = result.data()
@@ -125,5 +151,24 @@ def get_available_capacity_for_contract(contract_id=None,uri=None):
     return contractcapacity
 
 #assign_node_to_contract(node_id="compute-0-8", contract_id="hpc_bancarizada", share="50%", start_date=20170701)
-print get_available_capacity_for_contract("hpc_bancarizada")
+#print get_available_capacity_for_contract("hpc_bancarizada")
 
+
+def generate_slurm_credits_command(contract_id, days):
+
+    contract = get_contract(contract_id)
+    if not "uri" in contract.properties:
+        raise Exception("A contract to generate a SLURM command must have a SLURM URI")
+
+    uri = contract.properties["uri"]
+    slurm_data = get_slurm_data_from_uri(uri)
+    capacity = get_available_capacity_for_contract(contract_id=contract_id)
+    credit = capacity * days * 24
+
+    print "Command to assign credits to contract {0}".format(contract_id)
+    print "sbank-reset -c {0} -a {1}".format(slurm_data["cluster"], slurm_data["account"])
+    print "sbank-deposit -c {0} -a {1} -t {2}".format(slurm_data["cluster"], slurm_data["account"], credit)
+
+    pass
+
+generate_slurm_credits_command("hpc_bancarizada", 30)
